497bdedcd6fe17e1500c259c270cc533ac1ff25d
GitGeo07 <saraev.george@yandex.ru>
2026-05-07 19:48:56 +0300

    Very small progress on site(yeah...)

--- CatCollection.js
+++ CatCollection.js
@@ -1,7 +1,7 @@
 let cards = document.querySelector('.cards')
 
 let pics = ["Maxvell.gif",'CatDance.gif','cat-catfish.gif','Nword.png','SillyWilly.jpg','Xlebarsick.jpg','Swearing.jpg','unnamed.jpg','tenor.gif']
-let texts = ['Макс', 'Танцующий Андрей', "Виктор", "Слава(не черный)", "Филя", "Люся", "Коля", "Олег", "Питрович"]
+let texts = ['Максвелл - Редкость:Обычный, Мяукание:Редко, но метко, Способность:Крутится на 360, ломая закон физики и издавая музыку', 'Танцующий Андрей', "Виктор", "Слава(не черный)", "Филя", "Люся", "Коля", "Олег", "Питрович"]
 
 for (let i = 0; i < pics.length; i+=1) {
     let img = document.createElement('img');
